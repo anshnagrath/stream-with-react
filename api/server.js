46937c7a7d9cfa0059d02808ea1aac9228e76497
anshnagrath <anshnagrath448@gmail.com>
2019-04-30 11:31:34 +0530

    debuggingReact

---
+++ api/server.js
@@ -0,0 +1,11 @@
+
+module.exports = function (options) {
+    return function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        res.setHeader('Access-Control-Allow-Credentials', true);
+      // Implement the middleware function based on the options object
+      next()
+    }
+  }
